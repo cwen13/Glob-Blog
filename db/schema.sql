1f2cdbb7e45c0dfc039dc60881946e63adb044e4
cwen13 <cwenrich13@gmail.com>
2023-02-18 00:20:11 -0500

    starting base code

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS glob_blog_db;
+CREATE DATABASE glob_blog_db;
---
+++ models/Blog.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey:true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    description: {
+      type: DataType.STRING
+    },
+    body: {
+      type: DataType.STRING,
+      allowNull: false
+    },
+    author: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    comment_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'comment',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog',
+  }
+);
+
+module.exports =
